fa5b2df7fb45b684c1a89f9c50931598cfbdef29
Dmitry <dm.evsin@yandex.ru>
2024-10-17 12:07:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="userName" placeholder="Name">
+    <input type="password" v-model="userPass" placeholder="Password">
+    <input type="email" v-model="userEmail" placeholder="Email">
+    <button @click="sendData()">Send data</button>
+    <div v-if="users.length === 0" className="user">Список пуст</div>
+    <User v-for="(el, index) in users" :key="index" :user="el" @click="deleteUser(index)"/>
+  </div>
+</template>
+
+<script>
+import User from './components/User.vue'
+
+export default {
+  components: {User},
+  data() {
+    return {
+      users: [],
+      userName: '',
+      userPass: '',
+      userEmail: ''
+    }
+  },
+  methods: {
+    sendData() {
+      this.users.push({
+        name: this.userName,
+        pass: this.userPass,
+        email: this.userEmail
+      })
+    },
+    deleteUser(index){
+      this.users.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div className="user">
+        <p>{{ user.name }}</p>
+        <p>{{ user.email }} - ({{ user.pass }})</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.user {
+    width: 300px;
+    height: 80px;
+    border: 1px solid black;
+    background-color: aliceblue;
+    border-radius: 4px;
+    margin-bottom: 10px;
+    padding: 10px;
+}
+</style>
